6b22c60eca0ea2b7960c1f6f259c803b06063693
Doctor3131 <sirielwnf@gmail.com>
2025-10-07 15:43:54 +0700

    fresh start add few base config for debugging {i mean logging} and aoutomation on database connection

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const logger = require('./utils/logger')
+
+const { PORT } = require('./config/envConfig')
+
+const app = express()
+
+app.listen(PORT, () => {
+  logger.info(`Server is running on port ${PORT}`)
+})
+
+module.exports = app
